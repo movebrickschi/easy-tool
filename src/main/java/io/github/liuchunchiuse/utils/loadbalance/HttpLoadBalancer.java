5e18ec25f6fc3c25c2631061eff18b15830a3bd2
liuchunchi <liuchunchiuse@163.com>
2025-01-06 19:49:50 +0800

    feature:增加负载均衡

---
+++ src/main/java/io/github/liuchunchiuse/utils/loadbalance/HttpLoadBalancer.java
@@ -0,0 +1,18 @@
+package io.github.liuchunchiuse.utils.loadbalance;
+
+/**
+ * 负载均衡工具类
+ *
+ * @author Liu Chunchi
+ * @version 1.0
+ */
+public interface HttpLoadBalancer {
+
+
+    /**
+     * @param urls 包含权重的链接
+     * @return 返回的链接
+     */
+    String chooseDynamic(String urls);
+
+}

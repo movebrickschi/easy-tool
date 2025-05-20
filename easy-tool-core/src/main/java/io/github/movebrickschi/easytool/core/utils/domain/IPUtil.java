9c4a088138d2c3a585d50c42ecd71039a0fa63f2
liuchunchi <liuchunchiuse@163.com>
2025-05-20 15:10:34 +0800

    feat(core): 添加 IP 获取工具类和注解读取工具类 - 在 easy-tool-core 中新增 IPUtil 类，用于获取客户端 IP地址 - 新增 AnnotationReaderUtil 类，用于读取指定包下的注解信息 - 更新项目版本号至 3.0.5

---
+++ easy-tool-core/src/main/java/io/github/movebrickschi/easytool/core/utils/domain/IPUtil.java
@@ -0,0 +1,40 @@
+package io.github.movebrickschi.easytool.core.utils.domain;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * ip获取工具类
+ *
+ * @author Liu Chunchi
+ */
+public class IPUtil {
+
+    private IPUtil() {
+    }
+
+    private static final String UNKNOWN = "unknown";
+
+    public static String getClientIp(HttpServletRequest request) {
+        // 检查 X-Forwarded-For 头，这可能包含多个 IP 地址，用逗号分隔
+        String clientIp = request.getHeader("X-Forwarded-For");
+        if (clientIp == null || clientIp.isEmpty() || UNKNOWN.equalsIgnoreCase(clientIp)) {
+            // 如果 X-Forwarded-For 不存在或无效，尝试检查 X-Real-IP
+            clientIp = request.getHeader("X-Real-IP");
+            if (clientIp == null || clientIp.isEmpty() || UNKNOWN.equalsIgnoreCase(clientIp)) {
+                // 如果 X-Real-IP 也不存在或无效，使用 getRemoteAddr()
+                clientIp = request.getRemoteAddr();
+            }
+        } else {
+            // 取 X-Forwarded-For 中的第一个有效 IP 地址
+            String[] ipArray = clientIp.split(",");
+            for (String ip : ipArray) {
+                if (ip != null && !ip.isEmpty() && !UNKNOWN.equalsIgnoreCase(ip.trim())) {
+                    clientIp = ip.trim();
+                    break;
+                }
+            }
+        }
+        return clientIp;
+    }
+}
---
+++ easy-tool-core/src/main/java/io/github/movebrickschi/easytool/core/utils/reflect/AnnotationReaderUtil.java
@@ -0,0 +1,56 @@
+package io.github.movebrickschi.easytool.core.utils.reflect;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * AnnotationReaderUtil
+ * 使用示例：
+ * {@code
+ *
+ *  String packageName = "com.xxxx.Demo"; // 替换为你的包名
+ *         List<Field> fields = AnnotationReaderUtil.getAnnotationFields(packageName, AuditLogTarget.class);
+ * <p>
+ *         for (Field field : fields) {
+ *             AuditLogTarget label = field.getAnnotation(AuditLogTarget.class);
+ *             System.out.println("字段名: " + field.getName() + ", 标签值: " + label.value());
+ *         }
+ * <p>
+ * }
+ * @author Liu Chunchi
+ */
+public final class AnnotationReaderUtil {
+
+    private AnnotationReaderUtil() {
+    }
+
+    public static List<Class<?>> getClassesInPackage(String packageName) throws Exception {
+        // 注意：实际项目中需要动态扫描包下的类，可借助工具如Reflections库
+        List<Class<?>> classes = new ArrayList<>();
+        classes.add(Class.forName(packageName));
+        return classes;
+    }
+
+    /**
+     * 获取所有带有制定注解的字段
+     * @param packageName 实体类包名
+     * @param tClass 注解类
+     * @return 带有指定注解的字段
+     */
+    public static <T extends Annotation> List<Field> getAnnotationFields(String packageName, Class<T> tClass) throws Exception {
+        List<Class<?>> classes = getClassesInPackage(packageName);
+        List<Field> labeledFields = new ArrayList<>();
+
+        for (Class<?> clazz : classes) {
+            for (Field field : clazz.getDeclaredFields()) {
+                if (field.isAnnotationPresent(tClass)) {
+                    labeledFields.add(field);
+                }
+            }
+        }
+        return labeledFields;
+    }
+
+}
